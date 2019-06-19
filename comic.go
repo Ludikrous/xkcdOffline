c9e80aeaed0164d2e9c0e282685515beae1a5373
Dhanvee Ivaturi <dhanvee_ivaturi@symantec.com>
2019-06-19 11:21:03 -0700

    work more on comic type - add constructor

--- comic.go
+++ comic.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"image"
+)
+
 type comic struct {
+	number  int
 	address string
-	number  string
 	title   string
+	image   image.Image
+}
+
+func New(number int) (c comic) {
+	// initialize comic struct
+	c = comic{}
+	c.number = number
+	c.address = fmt.Sprintf("http://www.xkcd.com/%d", number)
+
+	// get data from xkcd servers
+	c.populateData()
+
+	// return populated struct
+	return c
+}
+
+func (c comic) populateData() {
+
 }
