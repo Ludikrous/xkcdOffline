b95a54dcea591f7f34248848c6052806ec0877f1
Dhanvee Ivaturi <dhanvee@umd.edu>
2019-06-20 10:57:40 -0700

    add some api support

--- comic.go
+++ comic.go
@@ -2,25 +2,67 @@ package main
 
 import (
 	"fmt"
+	jsoniter "github.com/json-iterator/go"
+	"log"
+	"net/http"
+	"time"
 )
+
+
+	//{
+	//"month": "6",
+	//"num": 2165,
+	//"link": "",
+	//"year": "2019",
+	//"news": "",
+	//"safe_title": "Millennials",
+	//"transcript": "",
+	//"alt": "Ironically, I've been having these same arguments for at least a decade now. I thought we would have moved on by now, but somehow the snide complaints about millennials continue.",
+	//"img": "https://imgs.xkcd.com/comics/millennials.png",
+	//"title": "Millennials",
+	//"day": "19"
+	//}
+
+
 
 type comic struct {
 	Number  int
-	Title   string
-	Caption string
-	Address string
+
+	Month int `json:"month"`
+	Day int `json:"day"`
+	Year int `json:"year"`
+
+	Title   string `json:"title"`
+	SafeTitle string `json:"safe_title"`
+	Caption string `json:"alt"`
+
+	Address string `json:"img"`
 }
 
-func New(number int) (c comic) {
+func NewComic(number int) (c comic) {
 	// initialize comic struct
 	c = comic{}
 	c.Number = number
 
-	comicURL := fmt.Sprintf("http://www.xkcd.com/%s", c.Title)
+	comicURL := fmt.Sprintf("http://www.xkcd.com/%d/info.0.json", number)
+	req, err := makeAPIRequest(comicURL)
 
 	return c
 }
 
-func (c comic) populateData() {
+func makeAPIRequest(url string) (req, error) {
 
+	c := http.Client{}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Set("User-Agent", "xkcdOffline")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
 }
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 )
 
+const URL string  = "https://xkcd.com/"
+const JSON_PATH string = "/info.0.json"
+
 func main() {
 	// define flags
 	pictureFolder := flag.String("loc", "./xkcd_comics/", "Full path for the folder in which comics will be stored")
@@ -12,9 +15,23 @@ func main() {
 	flag.Parse()
 
 	// populate picture folder with all the xkcd comics
-	getAllComics(pictureFolder)
+	allComics := getAllComics(pictureFolder)
 }
 
-func getAllComics(pictureFolder *string) {
+func getAllComics(pictureFolder *string) []comic {
+	// get latest comic number
+	currNumber := getHighestComicNum()
 
+	// make a slice of comics and populate them
+	allComics := make([]comic, currNumber)
+	// iterate through all comics
+	for num:=currNumber; num <= 0; num-- {
+		allComics[num] = NewComic(num)
+	}
+
+	return allComics
+}
+
+func getHighestComicNum() int {
+	return 2165		// TODO this method should be implemented later on
 }
